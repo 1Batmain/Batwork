3cd795d5f9077f41b6eac11c80d9994267d73600
BatMain <150813632+1Batmain@users.noreply.github.com>
2024-03-01 20:38:14 +0100

    Update script.js

--- SiteBatwork/script.js
+++ SiteBatwork/script.js
@@ -5,7 +5,7 @@ document.getElementById('send').addEventListener('click', function() {
     const userInput = document.getElementById('user-input').value;      // Récupère le texte saisi par l'utilisateur
 
     if (userInput.trim() !== '') {                                      // Vérifie que l'entrée n'est ps vide
-        fetch('http://localhost:3000/nvmsg', {                                               // Envoi une requête au serveur sur l'endpoint /nvmsg
+        fetch('/nvmsg', {                                               // Envoi une requête au serveur sur l'endpoint /nvmsg
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
