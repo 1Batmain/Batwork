3a98861cacfe8323971c58f5197f8ae261a7e76d
Achille <achille@laroche.16862>
2024-03-02 12:58:11 +0100

    catch fatal (uggly)

--- SiteBatwork/back_end/server.js
+++ SiteBatwork/back_end/server.js
@@ -23,89 +23,93 @@ app.listen(port, () => {
 
 // Création d'un endpoint sendMessage pour recevoir les messages du frontend
 app.post('/nvmsg', async (req, res) => {
-    console.log('Corps de la requête: ', req.body);
-    const userMessage = req.body.message;
-    console.log('Message reçu : ', userMessage);
+    try {
+      console.log('Corps de la requête: ', req.body);
+      const userMessage = req.body.message;
+      console.log('Message reçu : ', userMessage);
 
-    // ICI INTEGRATION DE LA LOGIQUE D'ENVOI DU MESSAGE A L'API ET ATTENDRE SA REPONSE
-    const openai = new OpenAI(({ apiKey: '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' })) ;
+      // ICI INTEGRATION DE LA LOGIQUE D'ENVOI DU MESSAGE A L'API ET ATTENDRE SA REPONSE
+      const openai = new OpenAI(({ apiKey: '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' })) ;
 
-    async function main() {
+      async function main() {
 
 
-        // Récupère mon assistant Batbot
-          const myAssistant = await openai.beta.assistants.retrieve(
-            "asst_x4H0sAdsNBL9vMqGr5kEjBF7"
-          );
-      
-      
-          // Crée un nouveau thread 
-          const emptyThread = await openai.beta.threads.create();
-      
-      
-          // Crée un message
-          const message = await openai.beta.threads.messages.create(
-            emptyThread.id,
-            {
-              role: "user",
-              content: userMessage
-            }
-          );
-      
-            // Lance la réponse de l'assistant 
-          const run = await openai.beta.threads.runs.create(
-            emptyThread.id,
-            { 
-              assistant_id: myAssistant.id,
-            }
-          );
-            // Verifie le statut de la réponse 
-          // Initialisation de 'statut' à un objet avec un 'status' initial fictif
-                  let statut = { status: 'initial' };
-      
-                  while (statut.status !== 'completed') {
-                    // Récupération du statut
-                    statut = await openai.beta.threads.runs.retrieve(emptyThread.id, run.id);
-      
-                    // Vérifiez ici si 'statut' a une structure différente et ajustez en conséquence
-      
-                    if (statut.status === 'completed') {
-                      console.log('Le processus est terminé.');
-                      break; // Sortie de la boucle si le statut est 'completed'
-                    } else {
-                      console.log("Batbot est en train  d'écrire...");
-                      await sleep(1000); // Attend 1 seconde avant de réessayer
-                    }
-                  }
-      
-          const messages = await openai.beta.threads.messages.list(
-            emptyThread.id
-          );
-      
-        console.log(message.content)
-        console.log(messages.data[0].content)
+          // Récupère mon assistant Batbot
+            const myAssistant = await openai.beta.assistants.retrieve(
+              "asst_x4H0sAdsNBL9vMqGr5kEjBF7"
+            );
         
-        const assistantResponse = {message:messages.data[0].content[0] };
-        res.json(assistantResponse); // Renvoi la réponse au frontend
+        
+            // Crée un nouveau thread 
+            const emptyThread = await openai.beta.threads.create();
+        
+        
+            // Crée un message
+            const message = await openai.beta.threads.messages.create(
+              emptyThread.id,
+              {
+                role: "user",
+                content: userMessage
+              }
+            );
+        
+              // Lance la réponse de l'assistant 
+            const run = await openai.beta.threads.runs.create(
+              emptyThread.id,
+              { 
+                assistant_id: myAssistant.id,
+              }
+            );
+              // Verifie le statut de la réponse 
+            // Initialisation de 'statut' à un objet avec un 'status' initial fictif
+                    let statut = { status: 'initial' };
+        
+                    while (statut.status !== 'completed') {
+                      // Récupération du statut
+                      statut = await openai.beta.threads.runs.retrieve(emptyThread.id, run.id);
+        
+                      // Vérifiez ici si 'statut' a une structure différente et ajustez en conséquence
+        
+                      if (statut.status === 'completed') {
+                        console.log('Le processus est terminé.');
+                        break; // Sortie de la boucle si le statut est 'completed'
+                      } else {
+                        console.log("Batbot est en train  d'écrire...");
+                        await sleep(1000); // Attend 1 seconde avant de réessayer
+                      }
+                    }
+        
+            const messages = await openai.beta.threads.messages.list(
+              emptyThread.id
+            );
+        
+          console.log(message.content)
+          console.log(messages.data[0].content)
           
+          const assistantResponse = {message:messages.data[0].content[0] };
+          res.json(assistantResponse); // Renvoi la réponse au frontend
+            
+        
+        
+        
+        // Fonction pour attendre un certain temps (en ms)
+        function sleep(ms) {
+          return new Promise(resolve => setTimeout(resolve, ms));
+        }
+        
+            }
+        
+        main();
       
       
       
-      // Fonction pour attendre un certain temps (en ms)
-      function sleep(ms) {
-        return new Promise(resolve => setTimeout(resolve, ms));
-      }
       
-          }
+      // Fin de la communication avec l'api 
       
-      main();
-    
-    
-    
-    
-    // Fin de la communication avec l'api 
-    
-    // Envoi d'une réponse (factice) au front-end
+      // Envoi d'une réponse (factice) au front-end
+    } catch (err) {
+      console.log("FATAL ERROR CRASH DODGED!?: " + err);
+    }
 
 });
 
