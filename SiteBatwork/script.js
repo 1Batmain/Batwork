ffbe0b357b2a68969139c296d87c9334723f9e7e
1Batmain <batduval@gmail.com>
2024-03-02 16:49:25 +0100

    Ajout de la logique d'affichage dans le script front end

--- SiteBatwork/script.js
+++ SiteBatwork/script.js
@@ -1,10 +1,26 @@
 
 
+function envoyerMessage() {
 
-document.getElementById('send').addEventListener('click', function() {
     const userInput = document.getElementById('user-input').value;      // Récupère le texte saisi par l'utilisateur
 
     if (userInput.trim() !== '') {                                      // Vérifie que l'entrée n'est ps vide
+        
+        
+        const userMessageElement = document.createElement('div');       // Crée un nouvel élément div pour afficher le message de l'utilisateur
+        userMessageElement.classList.add('nvmessage');                 // Ajoute la classe 'nvmessage' à l'élément créé
+        userMessageElement.textContent = userInput;                     // Ajoute le contenu à l'élément
+
+        const chatbox = document.getElementById('chatbox');             // Définie l'élement chatbox comme la div d'id 'chatbox' dans mon document
+        chatbox.appendChild(userMessageElement);                        // Ajoute userMessageElement à chatbox
+
+        chatbox.scrollTop = chatbox.scrollHeight;                       // Scroll 
+
+        
+        
+        
+        
+        
         fetch('http://127.0.0.1:3000/nvmsg', {                          // Envoi une requête au serveur sur l'endpoint /nvmsg
             method: 'POST',
             headers: {
@@ -16,6 +32,13 @@ document.getElementById('send').addEventListener('click', function() {
         .then(data => {
             console.log('Réponse reçue:', data);                        // Traite la réponse du serveur
 
+            const batbotResponseElement = document.createElement('div');       // Crée un nouvel élément div pour afficher la reponse de batbot
+            batbotResponseElement.classList.add('nvmessage');                 // Ajoute la classe 'nvmessage' à l'élément créé
+            batbotResponseElement.textContent = "Batbot : " + data.message.text.value;                     // Ajoute le contenu de la réponse à l'élément
+
+            chatbox.appendChild(batbotResponseElement);                        // Ajoute userMessageElement à chatbox
+
+            chatbox.scrollTop = chatbox.scrollHeight;                       // Scroll 
             // ICI : CODE POUR AFFICHER LA REPONSE DANS L'UI
 
         })
@@ -25,4 +48,13 @@ document.getElementById('send').addEventListener('click', function() {
     
         document.getElementById('user-input').value = '';               // Efface le champ de saisie
     }
-});
+};
+
+document.getElementById('send').addEventListener('click', envoyerMessage);
+
+document.getElementById('user-input').addEventListener('keydown',function(event) {
+    if(event.key === "Enter"){
+        event.preventDefault();
+        envoyerMessage();
+    }
+})
